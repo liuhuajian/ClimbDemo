660e0f7d600b4b1d075ab86556d4ad84df4a7701
liuhuajian <liuhuajian_lhj@163.com>
2018-09-30 00:21:15 +0800

    修改库名

--- src/ClimbControl.java
+++ src/ClimbControl.java
@@ -10,18 +10,14 @@ public class ClimbControl {
 	static final Log logger = LogFactory.getLog(ClimbControl.class);
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		//初始化一个httpclient
+		
         HttpClient client = new DefaultHttpClient();
-        //我们要爬取的一个地址，这里可以从数据库中抽取数据，然后利用循环，可以爬取一个URL队列
         String url="http://search.jd.com/Search?keyword=Python&enc=utf-8&book=y&wq=Python&pvid=33xo9lni.p4a1qb";
-        //抓取的数据
         List<JdModel> bookdatas=URLFecter.URLParser(client, url);
-        //循环输出抓取的数据
         for (JdModel jd:bookdatas) {
             logger.info("bookID:"+jd.getBookID()+"\t"+"bookPrice:"+jd.getBookPrice()+"\t"+"bookName:"+jd.getBookName()+"\t"+"bookUrl:"+jd.getBookDetailUrl());
         }
-        //将抓取的数据插入数据库
-//        MYSQLControl.executeInsert(bookdatas);
+        MYSQLControl.executeInsert(bookdatas);
 	}
 
 }
---
+++ src/MyDataSource.java
@@ -0,0 +1,15 @@
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class MyDataSource {
+	public static DataSource getDataSource(String connectURI){
+        BasicDataSource ds = new BasicDataSource();
+         //MySQL的jdbc驱动
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUsername("root");              //所要连接的数据库名
+        ds.setPassword("xyzroot12345");                //MySQL的登陆密码
+        ds.setUrl(connectURI);
+        return ds;
+    }
+}
